956818972b197d50bc9fc68fe93e5ef86d0f02ca
Akimi Yano <v-akimiyano@microsoft.com>
2020-12-16 16:06:04 -0800

    added snake class

---
+++ C-nake/Constants/InitialSnake.cs
@@ -0,0 +1,22 @@
+﻿using C_nake.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_nake.Constants
+{
+    class InitialSnake
+    {
+        // TODO: uncomment once its available
+        //public const Direction InitialDirection = Direction.Down;
+
+        // TODO: uncomment once we have MapCoordinate
+        // TODO: use variable for mapcordinate once we have map dimention constants 
+       
+        //public const MapCoordinate[] InitialBody = new MapCoordinate[] { 
+        //    new MapCoordinate(9, 19), 
+        //    new MapCoordinate(8, 19), 
+        //    new MapCoordinate(7, 19) 
+        //};
+    }
+}
--- C-nake/Models/Snake.cs
+++ C-nake/Models/Snake.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using C_nake.Constants;
 
 namespace C_nake.Models
 {
