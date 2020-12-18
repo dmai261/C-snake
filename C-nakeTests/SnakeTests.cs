eb6d54d9424eec4d432aa389e13c086e0a36e210
Justin Clayton (Aerotek US) <v-jclayton@microsoft.com>
2020-12-18 16:50:59 +0000

    Merged PR 21: Added a test for snake move method.
    Added a test for snake move method.

--- C-nakeTests/SnakeTests.cs
+++ C-nakeTests/SnakeTests.cs
@@ -9,40 +9,48 @@ namespace C_nakeTests
     [TestClass]
     public class SnakeTests
     {
+        private Map testMap;
         private Snake testSnake;
 
         [TestInitialize]
         public void Setup()
         {
-            Map newmap = new Map();
-            testSnake = new Snake(newmap);
+            testMap = new Map();
+            testSnake = new Snake(testMap);
         }
      
-
-     
         [TestMethod]
-        public void changeCurrentDiractionValidTest()
+        public void ChangeCurrentDiractionValidTest()
         {
             testSnake.CurrentDirection = ConsoleKey.LeftArrow;
             Assert.AreEqual(testSnake.CurrentDirection, ConsoleKey.LeftArrow);
         }
 
         [TestMethod]
-        public void invalidKeyInputForDirection()
+        public void InvalidKeyInputForDirection()
         {
             testSnake.CurrentDirection = ConsoleKey.W;
             Assert.AreNotEqual(testSnake.CurrentDirection, ConsoleKey.W);
         }
 
         [TestMethod]
-        public void invalidDirectionInputTest()
+        public void InvalidDirectionInputTest()
         {
             testSnake.CurrentDirection = ConsoleKey.DownArrow;
             testSnake.CurrentDirection = ConsoleKey.UpArrow;
             Assert.AreNotEqual(testSnake.CurrentDirection, ConsoleKey.UpArrow);
         }
 
-        
+        [TestMethod]
+        public void MoveTest()
+        {
+            MapCoordinate startingHeadCoord = InitialSnake.InitialBody[0];
+            MapCoordinate nextHeadCoord = new MapCoordinate(startingHeadCoord.Row + 1, startingHeadCoord.Col);
+            testSnake.CurrentDirection = ConsoleKey.DownArrow;
 
+            Assert.IsTrue(testSnake.Move());
+            Assert.IsTrue(testMap.GetTile(startingHeadCoord) is SnakeBodyTile);
+            Assert.IsTrue(testMap.GetTile(nextHeadCoord) is SnakeHeadTile);
+        }
     }
 }
